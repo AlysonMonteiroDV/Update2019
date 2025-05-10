e6c11a75132b76c45e472e5a0ff8a9c17014b8ed
AlysonMonteiroDV <alysonmonteironovo@gmail.com>
2025-05-10 10:42:06 -0300

    Firt Model-Controller-View Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departments> list = new List<Departments>();
+            list.Add(new Departments { id = 1, Name = "Eletronics"});
+            list.Add(new Departments { id = 2, Name = "Fashion"});
+
+
+            return View(list);
+        }
+    }
+}
